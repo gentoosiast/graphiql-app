7dcee576a0c02fa608982e3fafe2b01b331ffd6a
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-12-08 12:17:02 +0300

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,55 @@
 import type { JSX } from 'react';
 
+import { Button, ButtonGroup, ImageListItem, Link, Stack } from '@mui/material';
+
 export const Footer = (): JSX.Element => {
   return (
     <footer>
-      <p>Footer</p>
+      <Stack direction="row" justifyContent="space-around" sx={{ height: 60 }}>
+        <Link href="https://rs.school/react/">
+          <ImageListItem
+            alt="rss"
+            component="img"
+            src="src/assets/img/rs_school_js.svg"
+            sx={{ backgroundColor: '#fff', borderRadius: 2, width: 150 }}
+          />
+        </Link>
+        <ButtonGroup
+          color="primary"
+          orientation="horizontal"
+          size="large"
+          sx={{ width: 500 }}
+          variant="contained"
+        >
+          <Button fullWidth>
+            <Link
+              color="#fff"
+              href="https://github.com/gentoosiast"
+              sx={{ textDecoration: 'none' }}
+            >
+              Sergey
+            </Link>
+          </Button>
+          <Button fullWidth>
+            <Link
+              color="#fff"
+              href="https://github.com/Irina-Grebennikova"
+              sx={{ textDecoration: 'none' }}
+            >
+              Irina
+            </Link>
+          </Button>
+          <Button fullWidth>
+            <Link
+              color="#fff"
+              href="https://github.com/KateGoncharik"
+              sx={{ textDecoration: 'none' }}
+            >
+              Kate
+            </Link>
+          </Button>
+        </ButtonGroup>
+      </Stack>
     </footer>
   );
 };
--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -1,17 +1,23 @@
 import type { JSX } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { Box } from '@mui/material';
+
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 
 export const RootLayout = (): JSX.Element => {
   return (
     <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Header />
+        <main>
+          <Box component="main" sx={{ flexGrow: 1 }}>
+            <Outlet />
+          </Box>
+        </main>
+        <Footer />
+      </Box>
     </>
   );
 };
