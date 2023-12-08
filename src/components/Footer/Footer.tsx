45601e54f9a01e366690ef72ce3bd99407ad36b2
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-12-08 17:37:50 +0300

    fix: correct footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,19 @@
 import type { JSX } from 'react';
 
-import { Button, ButtonGroup, ImageListItem, Link, Stack } from '@mui/material';
+import { Button, ButtonGroup, Link, Stack } from '@mui/material';
 
 export const Footer = (): JSX.Element => {
   return (
     <footer>
-      <Stack alignItems="center" direction="row" justifyContent="space-around" sx={{ height: 60 }}>
+      <Stack
+        alignItems="center"
+        bgcolor="background.paper"
+        direction="row"
+        justifyContent="space-around"
+        sx={{ minHeight: 60 }}
+      >
         <Link href="https://rs.school/react/">
-          <ImageListItem
-            alt="rss"
-            component="img"
-            src="src/assets/img/rs_school_js.svg"
-            sx={{ backgroundColor: '#fff', borderRadius: 2, width: 125 }}
-          />
+          <img alt="RS School logo" src="/rss-logo.svg" style={{ maxHeight: 40 }} />
         </Link>
         <ButtonGroup
           color="primary"
@@ -22,26 +23,17 @@ export const Footer = (): JSX.Element => {
             boxShadow: 'none',
             display: 'flex',
             justifyContent: 'space-evenly',
-
             maxWidth: 600,
           }}
           variant="contained"
         >
-          <Link color="#fff" href="https://github.com/gentoosiast" sx={{ textDecoration: 'none' }}>
+          <Link href="https://github.com/gentoosiast" sx={{ textDecoration: 'none' }}>
             <Button sx={{ maxWidth: 200 }}>Sergey</Button>
           </Link>
-          <Link
-            color="#fff"
-            href="https://github.com/Irina-Grebennikova"
-            sx={{ textDecoration: 'none' }}
-          >
+          <Link href="https://github.com/Irina-Grebennikova" sx={{ textDecoration: 'none' }}>
             <Button sx={{ maxWidth: 200 }}>Irina</Button>
           </Link>
-          <Link
-            color="#fff"
-            href="https://github.com/KateGoncharik"
-            sx={{ textDecoration: 'none' }}
-          >
+          <Link href="https://github.com/KateGoncharik" sx={{ textDecoration: 'none' }}>
             <Button sx={{ maxWidth: 200 }}>Kate</Button>
           </Link>
         </ButtonGroup>
