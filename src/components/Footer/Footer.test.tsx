49ff5cc719baabbd308f8c273f34d9a5852d1b3c
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-12-10 16:20:59 +0300

    fix: correct import

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import { Footer } from '../Footer';
+import { Footer } from '.';
 
 describe('Footer', () => {
   it('should render footer', () => {
