f2a3ba9ffee44dbf79c5e6e5c497bc2523d5dc2b
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-12-10 16:08:08 +0300

    feat: add toggle language buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import type { JSX } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
-import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { AppBar, Button, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { Stack } from '@mui/system';
 
 import { I18NLanguage } from '@/config/i18n';
@@ -9,32 +10,48 @@ import { useI18NContext } from '@/providers/i18n';
 
 export const Header = (): JSX.Element => {
   const { setLanguage, translate } = useI18NContext();
+  const [localLanguage, setLocalLanguage] = useState('EN');
 
   return (
-    <header style={{}}>
-      <Stack
-        alignItems="center"
-        justifyContent="center"
-        sx={{ backgroundColor: 'primary.main', minHeight: 60, position: 'sticky', top: 0 }}
-      >
-        <Stack direction="row">
-          <ToggleButtonGroup>
-            <ToggleButton
-              color="secondary"
-              onClick={() => setLanguage(I18NLanguage.English)}
-              value={'EN'}
-            >
-              {translate('switchToEN')}
-            </ToggleButton>
-            <ToggleButton onClick={() => setLanguage(I18NLanguage.Russian)} value={'RU'}>
-              {translate('switchToRU')}
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </Stack>
-        <Stack sx={{ backgroundColor: 'primary.main', textDecoration: 'none' }}>
-          <Link to="/">Welcome page</Link>
-        </Stack>
+    <AppBar
+      sx={{
+        alignItems: 'center',
+        backgroundColor: 'primary.main',
+        justifyContent: 'center',
+        minHeight: 60,
+        position: 'sticky',
+        top: 0,
+      }}
+    >
+      <Stack direction="row">
+        <ToggleButtonGroup
+          exclusive
+          onChange={(e, nextLanguage) => setLocalLanguage(nextLanguage as string)}
+          value={localLanguage}
+        >
+          <ToggleButton
+            color="secondary"
+            onClick={() => setLanguage(I18NLanguage.English)}
+            value={'en'}
+          >
+            {translate('switchToEN')}
+          </ToggleButton>
+          <ToggleButton
+            color="secondary"
+            onClick={() => setLanguage(I18NLanguage.Russian)}
+            value={'ru'}
+          >
+            {translate('switchToRU')}
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
-    </header>
+      <Button
+        component={RouterLink}
+        sx={{ backgroundColor: 'primary.main', color: 'secondary.main', textDecoration: 'none' }}
+        to="/"
+      >
+        {translate('welcomePage')}
+      </Button>
+    </AppBar>
   );
 };
