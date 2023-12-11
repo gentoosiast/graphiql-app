58f9d507343e58b2a3d2a21001c7418414125a50
Sergey Koksharov <gentoosiastic@gmail.com>
2023-12-11 22:37:13 +0300

    fix: make main.tsx minimal again; create App and AppProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { RouterProvider } from 'react-router-dom';
 
-import { ThemeProvider } from '@emotion/react';
 import '@fontsource/philosopher';
-import { CssBaseline } from '@mui/material';
 
 import '@/config/firebase';
 
-import { I18NProvider } from './providers/i18n';
-import { router } from './routes';
-import { store } from './store';
-import theme from './theme';
+import { App } from './App';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -21,13 +14,6 @@ import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <I18NProvider>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </I18NProvider>
-    </Provider>
+    <App />
   </StrictMode>,
 );
