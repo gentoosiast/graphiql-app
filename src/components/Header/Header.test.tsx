10d34c89b288334e3122c0f0854c49d5a09a45fe
Kate <kate.goncharik.artist@gmail.com>
2023-12-13 18:15:03 +0300

    fix: remove provider from test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,11 +1,10 @@
-import { Provider } from 'react-redux';
+
 import { MemoryRouter } from 'react-router-dom';
 
 import { screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import { I18NProvider } from '@/providers/i18n';
-import { store } from '@/store';
 import { renderWithProviders } from '@/test/render-with-providers';
 
 import { Header } from '.';
@@ -13,17 +12,14 @@ import { Header } from '.';
 describe('Header', () => {
   it('should render header', () => {
     renderWithProviders(
-      <Provider store={store}>
         <I18NProvider>
           <MemoryRouter>
             <Header />
           </MemoryRouter>
         </I18NProvider>
-      </Provider>,
     );
 
     const welcomeLink = screen.getByRole('link');
-
     expect(welcomeLink).toBeInTheDocument();
   });
 });
