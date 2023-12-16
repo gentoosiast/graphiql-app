d1ed13800531acb561b1337b6226071b877520cc
Sergey Koksharov <gentoosiastic@gmail.com>
2023-12-16 22:21:43 +0300

    fix: remove debug info from AuthPage & change order of reexports in index.ts

--- src/features/auth/routes/AuthPage.tsx
+++ src/features/auth/routes/AuthPage.tsx
@@ -3,15 +3,12 @@ import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { Login, Register } from '../components';
-import { AuthState } from '../enums';
 import { getFormMode } from '../utils/get-form-mode';
 
 const AuthPage = (): JSX.Element => {
   const location = useLocation();
   const formMode = getFormMode(location.state);
   const [isLogin, setIsLogin] = useState(formMode === 'login');
-
-  console.log(AuthState.AUTHENTICATED);
 
   return isLogin ? <Login setIsLogin={setIsLogin} /> : <Register setIsLogin={setIsLogin} />;
 };
