4dc972ee61bf7276de8d0153e992503f34a2a1c2
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-12-18 11:46:16 +0300

    feat: add content to error page

--- src/features/misc/routes/ErrorPage.tsx
+++ src/features/misc/routes/ErrorPage.tsx
@@ -1,16 +1,49 @@
 import { useRouteError } from 'react-router-dom';
+
+import { Typography } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import { Box, Stack } from '@mui/system';
+
 export default function ErrorPage(): JSX.Element | undefined {
   const error = useRouteError();
   console.error(error);
   if (error instanceof Error) {
     return (
-      <div id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.message}</i>
-        </p>
-      </div>
+      <Box
+        alignItems={'center'}
+        display={'flex'}
+        flexDirection={'column'}
+        justifyContent={'center'}
+        width={'100%'}
+      >
+        <Typography align="center" component={'h1'} margin={'2%'} variant="h2">
+          Oops!
+        </Typography>
+
+        <Stack alignItems={'center'} justifyContent={'center'} width={'100%'}>
+          <Stack
+            alignItems={'center'}
+            justifyContent={'center'}
+            width={{ lg: '30%', md: '40%', sm: '80%', xs: '80%' }}
+          >
+            <img alt="cats-watch-laptop" src="/error-cats.jpg" style={{ width: '100%' }} />
+          </Stack>
+        </Stack>
+        <Typography
+          align="center"
+          component={'h3'}
+          fontSize={{ lg: 20, md: 18, sm: 18, xs: 16 }}
+          margin={'3% 2%'}
+          variant="h4"
+          width={'60%'}
+        >
+          Something went wrong.. Try to reload the page.
+        </Typography>
+
+        <Alert severity="error" sx={{ padding: 5, width: '90%' }}>
+          {error.message}
+        </Alert>
+      </Box>
     );
   }
 }
