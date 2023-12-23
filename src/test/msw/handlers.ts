fc0c23adef7adbcd981b5557379b06e8040af592
Sergey Koksharov <gentoosiastic@gmail.com>
2023-12-23 23:00:32 +0300

    feat: mock GraphQL introspection query with MSW

--- src/test/msw/handlers.ts
+++ src/test/msw/handlers.ts
@@ -1,1 +1,9 @@
-export const handlers = [];
+import { HttpResponse, graphql } from 'msw';
+
+import { introspectionResponse } from './mocks/introspectionResponse';
+
+export const handlers = [
+  graphql.query('IntrospectionQuery', () => {
+    return HttpResponse.json(introspectionResponse);
+  }),
+];
