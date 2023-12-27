2c2eb9277e2be711def5d23d78b6353860dfbdbe
Kate <kate.goncharik.artist@gmail.com>
2023-12-27 16:30:35 +0300

    fix: correct fallback test

--- src/components/Fallback/Fallback.test.tsx
+++ src/components/Fallback/Fallback.test.tsx
@@ -3,22 +3,32 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 
+import { I18NProvider } from '@/providers';
 import { renderWithProviders } from '@/test/renderWithProviders';
 
+import { Fallback } from '.';
 const TestComponent = (): JSX.Element => {
   throw new Error('Test Error');
 };
+
+const TestError: Error = new Error('Test Error');
+const FallbackWithProvider = (
+  <I18NProvider>
+    <Fallback error={TestError} resetErrorBoundary={() => {}} />
+  </I18NProvider>
+);
 
 describe('Fallback', () => {
   it('error should be caught by error boundary', async () => {
     vi.spyOn(console, 'error').mockImplementation(() => null);
 
     renderWithProviders(
-      <ErrorBoundary fallback={<>Error Boundary</>}>
+      <ErrorBoundary fallback={FallbackWithProvider}>
         <TestComponent />
       </ErrorBoundary>,
     );
 
-    expect(await screen.findByText('Error Boundary')).toBeVisible();
+    expect(await screen.findByText(/something went wrong.. try to reload the page/i)).toBeVisible();
+    expect(await screen.findByText(/test error/i)).toBeVisible();
   });
 });
