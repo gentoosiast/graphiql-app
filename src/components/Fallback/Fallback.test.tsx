2254a0a954d85c7ee4fa24cd53ee16aad14e9b31
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-12-28 19:57:18 +0300

    fix: apply sergey advices

--- src/components/Fallback/Fallback.test.tsx
+++ src/components/Fallback/Fallback.test.tsx
@@ -6,30 +6,25 @@ import { describe, expect, it, vi } from 'vitest';
 import { I18NProvider } from '@/providers';
 import { renderWithProviders } from '@/test/renderWithProviders';
 
-import { Fallback } from '.';
+import { Fallback } from './Fallback';
 
 const TestComponent = (): JSX.Element => {
   throw new Error('Test Error');
 };
-
-const TestError: Error = new Error('Test Error');
-const FallbackWithProvider = (
-  <I18NProvider>
-    <Fallback error={TestError} resetErrorBoundary={() => {}} />
-  </I18NProvider>
-);
 
 describe('Fallback', () => {
   it('error should be caught by error boundary', async () => {
     vi.spyOn(console, 'error').mockImplementation(() => null);
 
     renderWithProviders(
-      <ErrorBoundary fallback={FallbackWithProvider}>
-        <TestComponent />
-      </ErrorBoundary>,
+      <I18NProvider>
+        <ErrorBoundary FallbackComponent={Fallback}>
+          <TestComponent />
+        </ErrorBoundary>
+      </I18NProvider>,
     );
 
     expect(await screen.findByText(/something went wrong.. try to reload the page/i)).toBeVisible();
-    expect(await screen.findByText(/test error/i)).toBeVisible();
+    expect(screen.getByText(/test error/i)).toBeVisible();
   });
 });
