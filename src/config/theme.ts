8e3ef50ef9cd933a334b635a9b2dc61ab67784e9
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-12-29 10:37:22 +0300

    refactor: move fonts to config theme

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,6 +1,11 @@
 import { red } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export const theme = createTheme({
   breakpoints: {
     values: {
