f7cd50e804d3d063ab644b503a2f1a752f290970
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-12-29 11:05:39 +0300

    refactor: add font to theme config

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import '@fontsource/philosopher';
-
 import '@/config/firebase';
 
 import { App } from './App';
