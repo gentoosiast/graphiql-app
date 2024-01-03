ef6e2546c9b989afa0bc864e51fa9cd39186ca61
Sergey Koksharov <gentoosiastic@gmail.com>
2024-01-03 19:35:12 +0300

    fix: do not use type assertion in isI18NLanguage type guard

--- src/validators/isI18NLanguage.ts
+++ src/validators/isI18NLanguage.ts
@@ -1,5 +1,5 @@
 import { I18NLanguage } from '@/config/i18n';
 
 export const isI18NLanguage = (value: unknown): value is I18NLanguage => {
-  return Object.values(I18NLanguage).includes(value as I18NLanguage);
+  return Object.values(I18NLanguage).find((lang) => lang === value) !== undefined;
 };
