990764f847c41bafa85c7b10d7c486ba4c5ffd95
Sergey Koksharov <gentoosiastic@gmail.com>
2024-01-06 17:26:45 +0300

    feat: write test for language dropdown in the Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter } from 'react-router-dom';
 
 import { screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
 
 import { I18NProvider } from '@/providers/i18n';
@@ -20,5 +21,34 @@ describe('Header', () => {
 
     const welcomeLink = screen.getByRole('link');
     expect(welcomeLink).toBeInTheDocument();
+  });
+
+  it('should switch language with dropdown menu', async () => {
+    const user = userEvent.setup();
+
+    renderWithProviders(
+      <I18NProvider>
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      </I18NProvider>,
+    );
+
+    let languageEngDropdown: HTMLElement | null = screen.getByLabelText(/select ui language/i);
+    let languageRusDropdown = screen.queryByLabelText(/выбор языка интерфейса/i);
+
+    expect(languageEngDropdown).toBeInTheDocument();
+    expect(languageRusDropdown).not.toBeInTheDocument();
+
+    await user.click(languageEngDropdown);
+
+    const languageItem = screen.getByRole('menuitem', { name: /русский/i });
+    await user.click(languageItem);
+
+    languageEngDropdown = screen.queryByLabelText(/select ui language/i);
+    expect(languageEngDropdown).not.toBeInTheDocument();
+
+    languageRusDropdown = screen.getByLabelText(/выбор языка интерфейса/i);
+    expect(languageRusDropdown).toBeInTheDocument();
   });
 });
